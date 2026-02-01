52f83deeca7934e1b9ecaad77ad362ce2f70c839
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2026-02-01 13:37:04 +0530

    refactor: remove index.js files for layout and sections components

--- src/components/sections/index.js
+++
@@ -1,6 +0,0 @@
-export { default as AboutSection } from "./About/AboutSection";
-export { default as ContactSection } from "./ContactSection";
-export { default as EventSection } from "./EventSection";
-export { default as StatsSection } from "./About/StatsSection";
-export { default as TeamSection } from "./TeamSection";
-export { default as GallerySection } from "./GallerySection";
